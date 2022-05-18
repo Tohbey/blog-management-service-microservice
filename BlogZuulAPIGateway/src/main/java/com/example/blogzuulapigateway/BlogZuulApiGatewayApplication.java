efc5ac991e86efd9f13ce3c9168efe1c7ab80293
tohbey <fafoworatobi25@gmail.com>
2022-05-18 09:45:22 +0100

    Zuul API Gateway Setup

--- BlogZuulAPIGateway/src/main/java/com/example/blogzuulapigateway/BlogZuulApiGatewayApplication.java
+++ BlogZuulAPIGateway/src/main/java/com/example/blogzuulapigateway/BlogZuulApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.example.blogzuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class BlogZuulApiGatewayApplication {
 
     public static void main(String[] args) {
