a96321053adbcf1a3c05f3a0a77fa5183371b906
tohbey <fafoworatobi25@gmail.com>
2022-05-21 15:18:53 +0100

    Linking API config server to github repo

--- BlogAPIConfigServer/src/main/java/com/example/blogapiconfigserver/BlogApiConfigServerApplication.java
+++ BlogAPIConfigServer/src/main/java/com/example/blogapiconfigserver/BlogApiConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.example.blogapiconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BlogApiConfigServerApplication {
 
     public static void main(String[] args) {
