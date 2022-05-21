0d17ea1071419e366b62f834194d61c5bb8a0f8d
tohbey <fafoworatobi25@gmail.com>
2022-05-21 13:19:09 +0100

    setup of blog API config server

---
+++ BlogAPIConfigServer/src/main/java/com/example/blogapiconfigserver/BlogApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogapiconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiConfigServerApplication.class, args);
+    }
+
+}
---
+++ BlogAPIConfigServer/src/test/java/com/example/blogapiconfigserver/BlogApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
